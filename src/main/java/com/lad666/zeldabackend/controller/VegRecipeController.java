908f67e39242b83540cec69372505ec956f1e9dd
wackopastorius <ladaegolaeva@gmail.com>
2023-11-29 11:55:18 +0100

    cors

--- src/main/java/com/lad666/zeldabackend/controller/VegRecipeController.java
+++ src/main/java/com/lad666/zeldabackend/controller/VegRecipeController.java
@@ -3,6 +3,7 @@ package com.lad666.zeldabackend.controller;
 import com.lad666.zeldabackend.model.VegetarianRecipe;
 import com.lad666.zeldabackend.service.VegService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1")
 public class VegRecipeController {
     //properties
