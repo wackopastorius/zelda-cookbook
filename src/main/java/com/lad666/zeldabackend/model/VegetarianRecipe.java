3370e348fa0532044d0362440a4b92739d697986
wackopastorius <ladaegolaeva@gmail.com>
2023-12-02 20:47:03 +0100

    added pic url

--- src/main/java/com/lad666/zeldabackend/model/VegetarianRecipe.java
+++ src/main/java/com/lad666/zeldabackend/model/VegetarianRecipe.java
@@ -1,19 +1,50 @@
 package com.lad666.zeldabackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="veg_recipe")
-public class VegetarianRecipe extends Recipe{
+public class VegetarianRecipe {
+    //properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "recipe_name")
+    private String name;
+    @Column(name = "image_url")
+    private String imageURL;
 
     //constructors
     public VegetarianRecipe() {
     }
 
-    public VegetarianRecipe(int id, String name) {
-        super(id, name);
+    public VegetarianRecipe(int id, String name, String imageURL) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
     }
 
+    //get & set
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO veg_recipe (recipe_name) VALUES ('Creamy heart soup'),
-                                            ('Pumpkin stew'),
-                                            ('Veggie cream soup'),
-                                            ('Cream of mushroom soup');
+INSERT INTO veg_recipe (image_url, recipe_name) VALUES ('https://th.bing.com/th/id/OIG.ejc19TxD1dELmWagbSJd?pid=ImgGn','Creamy heart soup'),
+                                                       ('https://th.bing.com/th/id/OIG.VFksWssprOT0OfHY_daX?pid=ImgGn','Pumpkin stew'),
+                                                       ('https://th.bing.com/th/id/OIG.wh562TKQOOjbRdaUKRa4?pid=ImgGn','Veggie cream soup'),
+                                                       ('https://th.bing.com/th/id/OIG.1mYXSeIdNqMk8.gmp.GB?pid=ImgGn','Cream of mushroom soup');
 
 INSERT INTO seafood_recipe (image_url, recipe_name) VALUES ('https://th.bing.com/th/id/OIG.4Un32zGN4N0GpXgeufAn?pid=ImgGn', 'Clam chowder'),
                                                            ('https://th.bing.com/th/id/OIG.xm47kOVr23uFqCb9lGNG?pid=ImgGn', 'Creamy seafood soup'),
