9b7a3a091a0d0d9619c02d24de2440a81552ea53
MichaelGranbäck <michael.granback@outlook.com>
2023-12-02 21:31:01 +0100

    Fixed MeatRecipe model, now contains pic url attribute

--- src/main/java/com/lad666/zeldabackend/model/MeatRecipe.java
+++ src/main/java/com/lad666/zeldabackend/model/MeatRecipe.java
@@ -1,17 +1,57 @@
 package com.lad666.zeldabackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "meat_recipe")
-public class MeatRecipe extends Recipe{
+public class MeatRecipe {
 
-    //constructors
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "recipe_name")
+    private String name;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
     public MeatRecipe() {
     }
 
-    public MeatRecipe(int id, String name) {
-        super(id, name);
+    public MeatRecipe(String name, String imageURL) {
+        this.name = name;
+        this.imageURL = imageURL;
+    }
+
+    public MeatRecipe(int id, String name, String imageURL) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 }
